efecaf9c2a4247328c8809e55422e8939ff0f0c6
Casey McSpadden <casey@crossriver.com>
2013-01-31 13:18:29 -0600

    Initial mommit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demoapp::Application.config.secret_token = '5d3173f9ee58617b12e3c70bbc9f0903ea50a1effb41bc366a5d799bb6a7d2a65c1600bb808cb226de99da3009816cb85d1d166458494021fbc3fc0156fa6f63'
